7c37510f20613942d5ec7899edc11fa372d74ee1
mgkandeshwarath <mkandeshwara@umass.edu>
2023-11-11 01:40:22 -0500

    Connect to Mongo cluster, add method (template) to add people

--- mongo.py
+++ mongo.py
@@ -1,2 +1,18 @@
 from pymongo.mongo_client import MongoClient
-client = MongoClient("mongodb+srv://printscanner.mgav5zb.mongodb.net/")
+client = MongoClient("mongodb+srv://mkandeshwara:1234@printscanner.mgav5zb.mongodb.net/?retryWrites=true&w=majority")
+
+def addPerson(print, name, spid, email, type):
+    person = {
+        "print": print,
+        "name": name,
+        "SpireID": spid,
+        "email": email,
+        "type": type
+    }
+    db = client["people"]
+    if(type == "student"):
+        col = db["students"]
+        col.insert_one(person)
+    elif(type == "faculty"):
+        col = db["faculty"]
+        col.insert_one(person)
