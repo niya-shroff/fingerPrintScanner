77cf2efe8de71175edf0024ddd25960b8f8be48d
mgkandeshwarath <mkandeshwara@umass.edu>
2023-11-11 14:26:35 -0500

    something

--- frontend/src/components/SendFormData.jsx
+++ frontend/src/components/SendFormData.jsx
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const SendFormData = (data) => {
     axios({
-        method: "POST",
-        url: "http://127.0.0.1:5000",
+        method: "post",
+        url: "localhost:3000",
         data: data,
         withCredentials: false,
         headers: {
